881be9e77df3409180dd15f4ada25c8a12c9d0d4
Alex Mikhasenko <aleksei.mikhasenko@gmail.com>
2024-11-02 02:08:11 +0300

    Edit tests label

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using Test
 using PiPiBochum
 import PiPiBochum: mpi, c, ρ, amplitude
 
-@testset "Basic math" begin
+@testset "Test c and amplitude functions" begin
     @test imag(c(mpi, mpi, 0.3^2 + 0im)) ≈ -ρ(mpi, mpi, 0.3^2 + 0im)
     @test amplitude(1.1 + 0im) isa Matrix
 end
